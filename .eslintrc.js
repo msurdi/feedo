0913351909471c3cc0ab4bde4bc8a30591ac73e2
Matías Surdi <matiassurdi@gmail.com>
2021-05-28 17:40:03 +0200

    Setup editor, linter and formatter

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
