daaa1fe2e8bd236c6d60730663dee95d4c5c85b6
Matías Surdi <matiassurdi@gmail.com>
2021-06-04 07:48:43 +0200

    Setup test scripts and tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    "jest/globals": true,
   },
-  extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
+  extends: ["airbnb-base", "prettier", "plugin:jest/recommended"],
+  plugins: ["prettier", "jest"],
   parserOptions: {
     ecmaVersion: 12,
   },
