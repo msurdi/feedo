860e87696e007e35e5e271c9e22ba6558e52ae76
Matías Surdi <matiassurdi@gmail.com>
2021-06-04 07:49:15 +0200

    Add basic validation tests

---
+++ server/lib/validate.test.js
@@ -0,0 +1,38 @@
+const yup = require("yup");
+const validate = require("./validate");
+
+const testSchema = yup.object().shape({
+  url: yup.string().trim().url().required(),
+});
+
+describe("validate", () => {
+  describe("when there are validation errors", () => {
+    let validationResult;
+
+    beforeEach(async () => {
+      validationResult = await validate(testSchema, { url: "not_an_url" });
+    });
+    it("returns validation result in the expected format", () => {
+      expect(validationResult).toMatchObject({
+        errors: { url: ["url must be a valid URL"] },
+        values: { url: "not_an_url" },
+      });
+    });
+  });
+
+  describe("when there no are validation errors", () => {
+    let validationResult;
+
+    beforeEach(async () => {
+      validationResult = await validate(testSchema, {
+        url: "http://example.com",
+      });
+    });
+    it("returns validation result in the expected format", () => {
+      expect(validationResult).toMatchObject({
+        errors: null,
+        values: { url: "http://example.com" },
+      });
+    });
+  });
+});
