062efd9e2fd063589a06aa36afe407b85c020330
Matías Surdi <matiassurdi@gmail.com>
2021-06-07 07:46:21 +0200

    Avoid repeating eslint rule exception for each line

--- assets/compilers/up-disable.js
+++ assets/compilers/up-disable.js
@@ -1,13 +1,12 @@
+/* eslint-disable no-param-reassign */
+
 up.compiler("[up-disable]", (element) => {
   const { disableWith } = element.dataset;
   up.on(element, "click", () => {
     const originalWidth = element.style.width || element.offsetWidth;
     setTimeout(() => {
-      // eslint-disable-next-line no-param-reassign
       element.disabled = true;
-      // eslint-disable-next-line no-param-reassign
       element.style.width = `${originalWidth}px`;
-      // eslint-disable-next-line no-param-reassign
       element.textContent = disableWith ?? "Saving...";
     }, 0);
   });
