c4389eb24f9f4e2d5ae2befe28c3f63b058cfe13
Matías Surdi <matiassurdi@gmail.com>
2021-06-07 07:40:55 +0200

    Keep button width when showing loading state, fixes #5

--- assets/compilers/up-disable.js
+++ assets/compilers/up-disable.js
@@ -1,9 +1,12 @@
 up.compiler("[up-disable]", (element) => {
   const { disableWith } = element.dataset;
   up.on(element, "click", () => {
+    const originalWidth = element.style.width || element.offsetWidth;
     setTimeout(() => {
       // eslint-disable-next-line no-param-reassign
       element.disabled = true;
+      // eslint-disable-next-line no-param-reassign
+      element.style.width = `${originalWidth}px`;
       // eslint-disable-next-line no-param-reassign
       element.textContent = disableWith ?? "Saving...";
     }, 0);
