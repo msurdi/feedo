0d5c035d374fbe176c795c8413ce124524066b87
Matías Surdi <matiassurdi@gmail.com>
2021-06-18 06:53:52 +0200

    Disable unpoly banner

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,8 @@ import "unpoly";
 import "./compilers/up-disable";
 import "./compilers/up-progress";
 
+up.log.config.banner = false;
+
 if (process.env.NODE_ENV === "development") {
   up.log.enable();
 }
