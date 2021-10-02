8f3046490321a733d268c6fefdd376f9e4c39eb3
Matías Surdi <matiassurdi@gmail.com>
2021-10-02 09:08:47 +0200

    Add a default database path for Prisma
    This way, the application can be started without any configuration or environment variable. Closes #6.

--- bin/feedo.js
+++ bin/feedo.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
+const os = require("os");
+
+const homedir = os.homedir();
+
+// Ensure a default database is set for Prisma to work
+if (!process.env.FEEDO_DATABASE_URL) {
+  process.env.FEEDO_DATABASE_URL = `file:${homedir}/.feedo.db`;
+}
 
 // Prisma relies on relative directories for schema loading, etc.
 process.chdir(`${__dirname}/..`);
