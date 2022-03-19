90b29b11929e908443859808769ed23229dba305
Matías Surdi <matiassurdi@gmail.com>
2022-03-19 08:38:41 +0100

    Setup Next.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,35 @@
 module.exports = {
   env: {
-    browser: false,
-    commonjs: true,
+    browser: true,
     es2021: true,
     node: true,
     "jest/globals": true,
   },
-  extends: ["airbnb-base", "prettier", "plugin:jest/recommended"],
-  plugins: ["prettier", "jest"],
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "prettier",
+    "plugin:jest/recommended",
+  ],
+  plugins: ["react", "prettier", "jest"],
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 12,
+    sourceType: "module",
   },
   rules: {
     "no-restricted-syntax": "off",
     "no-continue": "off",
     "no-await-in-loop": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/function-component-definition": [
+      2,
+      { namedComponents: "arrow-function" },
+    ],
   },
 };
---
+++ pages/index.jsx
@@ -0,0 +1,3 @@
+const IndexPage = () => <h1>Hello feedo</h1>;
+
+export default IndexPage;
