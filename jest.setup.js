69934e5d06bed00e164a83fdea9daab12fac606a
Matías Surdi <matiassurdi@gmail.com>
2022-03-23 20:26:43 +0100

    Adapt unit tests to new project setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,29 +1,2 @@
 jest.mock("./lib/services/logger");
 jest.useFakeTimers();
-
-jest.mock("./server/config", () => ({
-  port: 8080,
-  address: "0.0.0.0",
-  auth: {
-    username: "unittestuser",
-    password: "unittestpassword",
-  },
-  rootDir: __dirname,
-  dataDir: `${__dirname}/data`,
-  publicRoot: `${__dirname}/public`,
-  reload: false,
-  helmet: {
-    contentSecurityPolicy: false,
-    hsts: false,
-  },
-  session: {
-    name: "s",
-    maxAge: 300000,
-    secure: false,
-    httpOnly: true,
-    keys: ["SECRET"],
-  },
-  feedo: {
-    oldestArticleDays: 10,
-  },
-}));
