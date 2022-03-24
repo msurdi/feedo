76795219272ee72c05ea83780f6aa31435d234de
Matías Surdi <matiassurdi@gmail.com>
2022-03-24 22:15:33 +0100

    Remove old assets

--- assets/app.js
+++
@@ -1,9 +0,0 @@
-import "unpoly";
-import "./compilers/up-disable";
-import "./compilers/up-submit-observer";
-
-up.log.config.banner = false;
-
-if (process.env.NODE_ENV === "development") {
-  up.log.enable();
-}
