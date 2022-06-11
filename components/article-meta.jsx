3924157880470605a1748e3509c590f14c5f901e
Matías Surdi <matiassurdi@gmail.com>
2022-06-11 12:03:12 +0200

    Display feed name in article's metadata

--- components/article-meta.jsx
+++ components/article-meta.jsx
@@ -1,7 +1,8 @@
 const ArticleMeta = ({ article }) => (
   <span className="text-xs text-gray-500">
     <time title={article.publishedAt}>{article.timeAgo}</time> ago
-    {article.author && <span>by {article.author}</span>}
+    {article.author && <span> by {article.author}</span>}
+    {article.feed?.name && <span> in {article.feed.name}</span>}
   </span>
 );
 
