67bcb9b38c850774c0ddf69e5cf6586390638a97
Matías Surdi <matiassurdi@gmail.com>
2022-06-17 19:34:45 +0200

    Make article rows take full width

--- components/article-list.jsx
+++ components/article-list.jsx
@@ -1,5 +1,5 @@
 const ArticleList = ({ children }) => (
-  <div className="max-w-full divide-y divide-gray-300">{children}</div>
+  <div className="w-full divide-y divide-gray-300">{children}</div>
 );
 
 export default ArticleList;
