99f3f6364a5c89fdd6497180ab5789cec02b66f6
Matías Surdi <matiassurdi@gmail.com>
2023-01-29 09:28:05 +0100

    Update TailwindCSS configuration to remove warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
-  purge: ["./server/views/**/*.{js,ts}"],
+  content: ["./server/views/**/*.{js,ts}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
